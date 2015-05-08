7f1502a8e6ff39440083be75b39d72f1c8df26d1
Sumit <sumitdas.usit@gmail.com>
2015-05-08 11:44:39 +0530

    changed line_length method to length

--- lib/application/line.rb
+++ lib/application/line.rb
@@ -6,7 +6,7 @@ class Application::Line
     @point_b = point_2
   end
 
-  def line_length
+  def length
     diff_x = ((@point_a.first - @point_b.first)).abs
     diff_x += 1 unless @point_a.first == 0 || @point_b.first == 0 || @point_a.first == @point_b.first
     diff_y = ((@point_a.last - @point_b.last)).abs
--- spec/application/line_spec.rb
+++ spec/application/line_spec.rb
@@ -4,22 +4,22 @@ describe Application::Line do
   context '#length' do
     it 'should return the correct length, 6' do
       line = Application::Line.new([1,0], [6,0])
-      expect(line.line_length).to eq(6)
+      expect(line.length).to eq(6)
     end
 
     it 'should return the correct length, 7' do
       line = Application::Line.new([1,0], [7,0])
-      expect(line.line_length).to eq(7)
+      expect(line.length).to eq(7)
     end
 
     it 'should return the correct length, 5' do
       line = Application::Line.new([1,0], [4,3])
-      expect(line.line_length).to eq(5)
+      expect(line.length).to eq(5)
     end
 
     it 'should return the correct length, 5' do
       line = Application::Line.new([1,5], [5,5])
-      expect(line.line_length).to eq(5)
+      expect(line.length).to eq(5)
     end
   end
 
